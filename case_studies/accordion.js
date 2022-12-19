9804336bdc208b715936e959a5359022ff578be9
Yerlan Jumabayev <thevarp18@gmail.com>
2022-12-19 23:16:51 +0600

    add login and some changes

--- case_studies/accordion.js
+++ case_studies/accordion.js
@@ -5,3 +5,20 @@ for (i=0; i<accordion.length; i++) {
     this.classList.toggle('active')
   })
 }
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight+100;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+       if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add("active");
+       }
+      }
+  }
+  window.addEventListener("scroll", reveal);
+  $('#bur1').click(function(){
+    var width = $(window).width();
+    $('#bur1').toggleClass("open");
+    $('#ov1').slideToggle();
+});   
